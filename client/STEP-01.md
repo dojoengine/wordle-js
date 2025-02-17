f072d6335f35d144720811a699604568f08b1ffa
Valentin Dosimont <valentin@pupucecorp.com>
2025-02-17 08:43:18 +0100

    feat: add dependencies

---
+++ client/STEP-01.md
@@ -0,0 +1,7 @@
+# STEP 01
+
+In client folder install dependencies 
+```bash
+pnpm add @dojoengine/sdk @dojoengine/core
+pnpm add -D vite-plugin-top-level-await vite-plugin-wasm @types/node
+```
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,26 @@
 import { defineConfig } from 'vite'
+import * as path from "node:path";
 import react from '@vitejs/plugin-react'
+import wasm from 'vite-plugin-wasm';
+import topLevelAwait from 'vite-plugin-top-level-await';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), wasm(), topLevelAwait()],
+  build: {
+    target: "esnext",
+    rollupOptions: {
+        // Ensure external modules are properly handled
+        external: ["chalk"],
+        output: {
+            format: "esm",
+        },
+    },
+},
+resolve: {
+  alias: {
+      "@": path.resolve(__dirname, "./src"),
+  },
+},
+
 })
